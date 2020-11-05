f181d70720c7dbe23da6f1d1364f81de58dfaa5b
Yuvraaj Narula <ynarula678@gmail.com>
2020-11-05 22:50:08 +0530

    js fixes

---
+++ app.js
@@ -0,0 +1,14 @@
+//selectors
+
+const input = document.querySelector('.todo-input');
+const button = document.querySelector('.todo-btn');
+const list = document.querySelector('.list');
+
+//events
+button.addEventListener('click', addTodo);
+
+//function 
+
+function addTodo(event){
+    console.log('Hello');
+}
