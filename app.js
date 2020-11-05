56339d4c9733095478f322d2835b83650b8c32b2
Yuvraaj Narula <ynarula678@gmail.com>
2020-11-05 23:01:44 +0530

    js update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //selectors
-
 const input = document.querySelector('.todo-input');
 const button = document.querySelector('.todo-btn');
 const list = document.querySelector('.list');
@@ -8,7 +7,23 @@ const list = document.querySelector('.list');
 button.addEventListener('click', addTodo);
 
 //function 
-
 function addTodo(event){
-    console.log('Hello');
+    event.preventDefault();
+    // creating todo div
+    const div = document.createElement('div');
+    div.classList.add('todo');
+    const newtodo = document.createElement('li');
+    newtodo.innerText = 'HEY';
+    newtodo.classList.add('item');
+    div.appendChild(newtodo);
+    //check btn
+    const completed = document.createElement('button');
+    completed.innerHTML = '<i class="fa fa-check"></i>';
+    completed.classList.add('completed-btn');
+    div.appendChild(completed);
+    //trash btn
+    const trash = document.createElement('button');
+    trash.innerHTML = '<i class="fa fa-trash"></i>';
+    trash.classList.add('trash-btn');
+    div.appendChild(trash);
 }
